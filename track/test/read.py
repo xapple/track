b13469f4ee1186763ef7e0f4e272171668f08620
Lucas Sinclair <lucas.sinclair@me.com>
2011-11-21 17:34:09 +0100

    Added a basic read test

--- track/test/read.py
+++ track/test/read.py
@@ -17,16 +17,28 @@ except ImportError:
     import unittest
 
 # Nosetest flag #
-__test__ = False
+__test__ = True
 
 ###################################################################################
-class TestRead(unittest.TestCase):
+class TestWithChrom(unittest.TestCase):
+    """Read all features from a track"""
     def runTest(self):
-        in_path = samples['features'][1]['sql']
+        in_path = samples['small_features'][1]['sql']
         with track.load(in_path) as t:
-            data = t.read('chr1', fields=['start','end','attributes'])
+            data = t.read()
             got = list(data)
-        expected = []
+        expected = [('chrI', 0, 10, u'Validation feature 1', 10.0),
+                    ('chrI', 2, 8, u'Validation feature 2', 0.0),
+                    ('chrI', 20, 30, u'Validation feature 3', 10.0),
+                    ('chrI', 25, 30, u'Validation feature 4', 0.0),
+                    ('chrI', 40, 45, u'Validation feature 5', 0.0),
+                    ('chrI', 40, 50, u'Validation feature 6', 10.0),
+                    ('chrI', 60, 70, u'Validation feature 7', 10.0),
+                    ('chrI', 70, 80, u'Validation feature 8', 10.0),
+                    ('chrI', 90, 100, u'Validation feature 9', 10.0),
+                    ('chrI', 90, 110, u'Validation feature 10', 10.0),
+                    ('chrI', 120, 130, u'Validation feature 11', 10.0),
+                    ('chrI', 125, 135, u'Validation feature 12', 5.0)]
         self.assertEqual(got, expected)
 
 #-----------------------------------#
