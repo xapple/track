f560c26324dcb01b3a522b6dcad4a87715014f09
Lucas Sinclair <lucas.sinclair@me.com>
2011-12-21 13:02:51 +0100

    Added an extra test

---
+++ track/test/convert.py
@@ -0,0 +1,35 @@
+"""
+Contains format conversion tests.
+"""
+
+# Built-in modules #
+import os
+
+# Internal modules #
+import track
+from track.test import samples
+from track.common import temporary_path, assert_file_equal
+
+# Unittesting module #
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# Nosetest flag #
+__test__ = True
+
+###################################################################################
+class TestSQLtoBED(unittest.TestCase):
+    """Convert a file to BED"""
+    def runTest(self):
+        in_path = samples['small_features'][1]['sql']
+        out_path = temporary_path('.bed')
+        track.convert(in_path, out_path, assembly='sacCer2')
+        #assert_file_equal(out_path, samples['small_features'][1]['bed'])
+
+#-----------------------------------#
+# This code was written by the BBCF #
+# http://bbcf.epfl.ch/              #
+# webmaster.bbcf@epfl.ch            #
+#-----------------------------------#
