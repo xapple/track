43b1ad68a2953613019b54b8766e19ee07f8f451
Yohan Jarosz <yohanjarosz@yahoo.fr>
2011-12-30 14:08:20 +0100

    error : cannot initialize Parser without a Serializer

--- track/parse/__init__.py
+++ track/parse/__init__.py
@@ -55,8 +55,11 @@ class Parser(object):
     def __call__(self, handler=None):
         # Default handler #
         if not handler:
-            from track.seralize import Serializer
-            handler = Serializer()
+            raise SyntaxError('must be call with a Serializer as first parameter')
+#            cannot work : Serializer take a path as first param
+#            from track.seralize import Serializer
+#            handler = Serializer()
+
         # Enter the handler #
         with handler as self.handler: self.parse()
         # Return a list of paths or a single path #
