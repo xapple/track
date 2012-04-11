c573e594994fe865a8140ead2ea5c2a4d0b55617
Julien Delafontaine <julien.delafontaine@epfl.ch>
2012-04-11 12:39:48 +0200

    wrong header for bedGraph (must have capital G)

--- track/serialize/bedgraph.py
+++ track/serialize/bedgraph.py
@@ -39,7 +39,7 @@ class SerializerBedgraph(Serializer):
 
     def newTrack(self, info=None, name=None):
         if not info: info = {}
-        info['type'] = 'bedgraph'
+        info['type'] = 'bedGraph'
         info['converted_by'] = __package__
         self.file.write("track " + ' '.join([k + '="' + v + '"' for k, v in info.items()]) + '\n')
         self.tracks.append(self.path)
