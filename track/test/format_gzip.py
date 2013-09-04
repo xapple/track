d1c129357d5b4390b098708b045c354948f6b2d7
bow <bow@bow.web.id>
2013-09-05 00:33:14 +0200

    Remove gzip without extension test
    gzip should not be treated as a format of its own as it applies to
    all genomic tracks format. Thus, we can dispense with the test
    case for gzip files without an extension.

--- track/test/format_gzip.py
+++ track/test/format_gzip.py
@@ -31,14 +31,3 @@ class TestConversion(unittest.TestCase):
         self.assertTrue(assert_file_equal(orig_sql_path, test_sql_path))
         # Clean up #
         os.remove(test_sql_path)
-
-class TestWithoutExtension(unittest.TestCase):
-    def runTest(self):
-        in_path = samples['gzip_tracks'][4]['gzip']
-        with track.load(in_path) as t:
-            data = t.read()
-            got = list(data)
-        expected = [('chr1', 10, 20, u'Lorem', 1.0, 1),
-                    ('chr1', 30, 40, u'Ipsum', 2.0, 1),
-                    ('chr2', 10, 20, u'Dolor', 3.0, 1)]
-        self.assertEqual(got, expected)
